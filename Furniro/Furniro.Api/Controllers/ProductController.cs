f446701aa595020d54671ae5587157023eb980a6
ShadowCode007 <plamen24yavor@gmail.com>
2024-09-06 10:03:22 +0300

    Add product controller

---
+++ Furniro/Furniro.Api/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Furniro.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            return Ok("HelloWorld");
+        }
+    }
+}
