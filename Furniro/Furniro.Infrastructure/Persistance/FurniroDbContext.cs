2829265e648cc9b8a281782fd2769db40821334d
ShadowCode007 <plamen24yavor@gmail.com>
2024-09-07 16:20:56 +0300

    Change context builds

--- Furniro/Furniro.Infrastructure/Persistance/FurniroDbContext.cs
+++ Furniro/Furniro.Infrastructure/Persistance/FurniroDbContext.cs
@@ -1,5 +1,6 @@
 using Furniro.Domain.Aggregates;
 using Furniro.Domain.Enteties;
+using Furniro.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 public class FurniroDbContext : DbContext
@@ -15,7 +16,24 @@ public class FurniroDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+
         modelBuilder.Entity<Order>()
             .HasMany(o => o.Products);
+
+        modelBuilder.Entity<Order>()
+        .OwnsOne(o => o.Address, a =>
+        {
+            a.Property(a => a.City);
+            a.Property(a => a.Country);
+            a.Property(a => a.AddressLine);
+            a.Property(a => a.PostalCode);
+        });
+
+        modelBuilder.Entity<Order>()
+        .OwnsOne(o => o.Client, c =>
+        {
+            c.Property(c => c.FirstName);
+            c.Property(c => c.LastName);
+        });
     }
 }
